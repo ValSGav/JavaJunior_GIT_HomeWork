43aff4599a4b8f49a59dce1593aa876b0595fc3d
Valentin developer <buziza@yandex.ru>
2024-12-04 17:54:43 +0300

    lesson3 homework step 3

--- src/main/java/ru/gb/lesson3/homework/App.java
+++ src/main/java/ru/gb/lesson3/homework/App.java
@@ -1,5 +1,8 @@
 package ru.gb.lesson3.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     /*
     Разработайте класс Student  с полями String name, int age, transient double GPA(средний балл)
@@ -10,4 +13,16 @@ public class App {
     Выведите все поля объекта, ключая GPA и обсудите, почему значение GPA не было сохранено/восстановлено
     ++ реализовать сериализацию json и  xml
     * */
+
+    List<Student> students = new ArrayList<>();
+
+    public static List<Student> generateListOfStudents(){
+        List<Student> generatesStudents= new ArrayList<>();
+        generatesStudents.add(new Student("Alex", 26));
+        generatesStudents.add(new Student("Mary", 21));
+        generatesStudents.add(new Student("Suzi", 17));
+        generatesStudents.add(new Student("Killian", 29));
+        return generatesStudents;
+    }
+
 }
---
+++ src/main/java/ru/gb/lesson3/homework/Student.java
@@ -0,0 +1,51 @@
+package ru.gb.lesson3.homework;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    //region Поля
+    private String name;
+    private int age;
+    transient double gpa;
+    //endregion
+    //region Конструкторы
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.gpa = 0d;
+    }
+    //endregion
+//region Методы
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder( this )
+                .append( "name", name )
+                .append( "age", age )
+                .append( "gpa", gpa )
+                .toString();
+    }
+//endregion
+
+}
